7c35cfbe78dfad9d26913084e2a006b955098968
Fen1k <fen1k.1234567@gmail.com>
2025-11-26 21:02:37 +0100

    steam keyword fixed

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ while True: # main cycle
     [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in time if stdin == i] # time
     [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in date if stdin == i] # date
     [[print('No problem!'), os.system('firefox')] for i in firefox if stdin == i] # open firefox
-    [[print('No problem!'), os.system('steam')] for i in firefox if stdin == i] # open steam
+    [[print('No problem!'), os.system('steam')] for i in steam if stdin == i] # open steam
     [os.system('clear') if stdin == 'clear' or stdin == 'cls' else None] # clear terminal
     if stdin == 'exit': # exit to terminal
         break
