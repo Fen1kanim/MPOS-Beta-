d21be54a52f4e02658c9b7d37e1029e2900e98f1
Fen1k <fen1k.1234567@gmail.com>
2025-11-27 15:56:27 +0100

    fixed wrong json filename

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import datetime
 import os
 import json
 
-with open("keywords", "r") as jf:
+with open("keywords.json", "r") as jf:
     js = json.load(jf)
 
 while True: # main cycle
     stdin = input("--> ")
-    [print('Hi! How r u?') for i in jf["hi"] if stdin == i] # hi
+    [print('Hi! How r u?') for i in js["hi"] if stdin == i] # hi
     [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in js["time"] if stdin == i] # time
     [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in js["date"] if stdin == i] # date
     [[print('No problem!'), os.system('firefox')] for i in js["firefox"] if stdin == i] # open firefox
