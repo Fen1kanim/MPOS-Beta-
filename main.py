c172da0d51402bbdb32e65bd9705bd05d3ce9b55
Fen1k <fen1k.1234567@gmail.com>
2025-12-04 15:50:14 +0100

    moved all itializations to ./python/request.py

--- main.py
+++ main.py
@@ -20,26 +20,4 @@ print("print help to list comands")
 print()
 
 while True: # main cycle
-    stdin = input("--> ") # as user for any commands
-
-    [call(["python", "./python/help.py"]) if stdin == 'help' else None] # help
-
-    [print('you are as', lastUser["name"], 'authorized') if stdin == 'whoami' else None]
-
-    [print('Hi! How r u?') for i in keywords["hi"] if stdin == i] # hi
-
-    [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in keywords["time"] if stdin == i] # time
-
-    [call(['python', './python/delete.py']) for i in keywords["delete"] if stdin == i]
-
-    [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in keywords["date"] if stdin == i] # date
-
-    [os.system('clear') if stdin == 'clear' or stdin == 'cls' else None]
-
-    [call(["python", "./python/open.py"]) for i in keywords["open"] if stdin == i] # open
-
-    [call(["python", "./python/calc.py"]) for i in keywords["calc"] if stdin == i] # calc
-
-    if stdin == 'exit': # exit to terminal
-        print("byeee")
-        break
+    call(['python', './python/request.py'])
