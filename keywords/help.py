d0087da735824d5fca3652db383fe4f34c7268c1
Kirillpro3000 <gricajkirill59@gmail.com>
2025-12-09 14:42:55 +0100

    added uranium keyword description

--- keywords/help.py
+++ keywords/help.py
@@ -8,5 +8,6 @@ help - list commands, you use it right now
 hi - just say hi! Be polite, i spend around 2 weeks for this shit
 open - types a command to terminal
 whoami - shows you how are you authentificated as
-delete - delete youre account''')
+delete - delete youre account
+uranium - irradiates you''')
 print()
