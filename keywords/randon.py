cb323dfc09ea80fc0ccc09ed0f3ad0cd5b68ca4f
Fen1k <fen1k.1234567@gmail.com>
2025-12-09 17:20:39 +0100

    randon.py now works like a game

--- keywords/randon.py
+++ keywords/randon.py
@@ -1,4 +1,14 @@
 import random
 
-num = random.randint(0, 1000000)
-print('the random number is',num,'!')
+num = str(random.randint(0, 10))
+while True:
+    stdin = input('guess the number: ')
+    if stdin == num:
+        print('the random number is',num,'!')
+        break
+    elif int(stdin) < int(num):
+        print('the number is bigger')
+    elif int(stdin) > int(num):
+        print('the number is smaler')
+    else:
+        print('it is not a number')
