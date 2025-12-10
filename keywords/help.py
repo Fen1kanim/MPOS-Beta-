777840d951aa717d99ac190e36291228f52418f6
Kirillpro3000 <gricajkirill59@gmail.com>
2025-12-10 14:47:07 +0100

    added pong and doom  keyword description

--- keywords/help.py
+++ keywords/help.py
@@ -9,5 +9,10 @@ hi - just say hi! Be polite, i spend around 2 weeks for this shit
 open - types a command to terminal
 whoami - shows you how are you authentificated as
 delete - delete youre account
-uranium - irradiates you''')
+uranium - irradiates you
+pong - play pong alone or with your friend. it has two more variables after pong and they are:
+1. points to win
+2. paddle lenght
+for ex. pong 10 6 will make the points to win to 10 and the paddle lenght to 6
+doom - play a simple doom like game''')
 print()
