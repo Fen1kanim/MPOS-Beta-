57e7df77f76d4eda2e45b7fd630490f78e64fc61
Face <fen1k.1234567@gmail.com>
2025-12-29 12:10:37 +0100

    also made the game func in menu.py

--- keywords/games/menu.py
+++ keywords/games/menu.py
@@ -1,20 +1,20 @@
 from subprocess import call
 
-stdin = input('''
+def game():
+    stdin = input('''
 1. 2.5D labyrinth
 2. guess a random number
 3. pong
 4. rock paper scissors
 5. snake
 6. tic tac toe
-7. flappy bird
-
+7. flappy bird \n
 choose a game(1234567): ''')
 
-[call(["python", "./keywords/games/labyrinth.py"]) if stdin == "1" else None]
-[call(["python", "./keywords/games/guess.py"]) if stdin == '2' else None]
-[call(["python", "./keywords/games/pong.py"]) if stdin == '3' else None]
-[call(["python", "./keywords/games/rps.py"]) if stdin == '4' else None]
-[call(["python", "./keywords/games/snake.py"]) if stdin == '5' else None]
-[call(["python", "./keywords/games/tictactoe.py"]) if stdin == '6' else None]
-[call(["python", "./keywords/games/flappy_bird.py"]) if stdin == '7' else None]
+    [call(["python", "./keywords/games/labyrinth.py"]) if stdin == "1" else None]
+    [call(["python", "./keywords/games/guess.py"]) if stdin == '2' else None]
+    [call(["python", "./keywords/games/pong.py"]) if stdin == '3' else None]
+    [call(["python", "./keywords/games/rps.py"]) if stdin == '4' else None]
+    [call(["python", "./keywords/games/snake.py"]) if stdin == '5' else None]
+    [call(["python", "./keywords/games/tictactoe.py"]) if stdin == '6' else None]
+    [call(["python", "./keywords/games/flappy_bird.py"]) if stdin == '7' else None]
